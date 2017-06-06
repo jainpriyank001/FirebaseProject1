7535168c22a38c5778e602e59f90d0e446a9f3a9
Priyank Jain <priyankjain@Priyanks-MacBook-Pro.local>
2017-06-06 14:33:48 +0530

    retrieve data

--- app/src/main/java/com/nearbuy/priyankjain/firebaseproject1/MainActivity.java
+++ app/src/main/java/com/nearbuy/priyankjain/firebaseproject1/MainActivity.java
@@ -5,58 +5,83 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.Editable;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
 import java.util.HashMap;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Button mFirebasebtn;
-
-    private EditText mNameField;
-    private EditText mEmailField;
 
     private DatabaseReference mDatabase;
+
+    private ListView mUserList;
+    private ArrayList<String> mUserName=new ArrayList<>();
+    private ArrayList<String> mkeys=new ArrayList<>();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mFirebasebtn=(Button) findViewById(R.id.firebaseBtn);
-        mDatabase = FirebaseDatabase.getInstance().getReference();
+        mDatabase = FirebaseDatabase.getInstance().getReference().child("User1");
+        mUserList=(ListView) findViewById(R.id.user_list);
 
-        mNameField=(EditText) findViewById(R.id.editText);
-        mEmailField=(EditText)findViewById(R.id.editText2);
-        mFirebasebtn.setOnClickListener(new View.OnClickListener(){
+        final ArrayAdapter<String> arrayAdapter=new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1  , mUserName );
+
+        mUserList.setAdapter(arrayAdapter);
+        mDatabase.addChildEventListener(new ChildEventListener() {
             @Override
-            public void onClick(View v){
-            //create child in root object
-            // Assign some value to child object
-                String name=mNameField.getText().toString().trim();
-                String email=mEmailField.getText().toString().trim();
-                HashMap<String,String> dataMap=new HashMap<String,String>();
-                dataMap.put("Name",name);
-                dataMap.put("Email",email);
+            public void onChildAdded(DataSnapshot dataSnapshot, String s) {
 
-                mDatabase.push().setValue(dataMap).addOnCompleteListener(new OnCompleteListener<Void>() {
-                    @Override
-                    public void onComplete(@NonNull Task<Void> task) {
-                        if(task.isSuccessful())
-                        {
-                            Toast.makeText(MainActivity.this,"Stored Successfully",Toast.LENGTH_LONG).show();
-                        }
-                        else
-                            Toast.makeText(MainActivity.this,"error in storing",Toast.LENGTH_LONG).show();
+                String value=dataSnapshot.getValue(String.class);
+                mUserName.add(value);
 
+                String key=dataSnapshot.getKey();
+                mkeys.add(key);
 
-                    }
-                });
+                arrayAdapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onChildChanged(DataSnapshot dataSnapshot, String s) {
+
+                    String value=dataSnapshot.getValue(String.class);
+                    String key=dataSnapshot.getKey();
+
+                    int index=mkeys.indexOf(key);
+                    mUserName.set(index,value);
+                    arrayAdapter.notifyDataSetChanged();
+
+            }
+
+            @Override
+            public void onChildRemoved(DataSnapshot dataSnapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(DataSnapshot dataSnapshot, String s) {
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
             }
         });
     }
